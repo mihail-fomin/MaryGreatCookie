990692dec9f1a2e78e071d08a55f0c98cfe861fe
misha133kv <mihail_fomin_95@mail.ru>
2023-04-19 19:45:23 +0500

    edited styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import 'normalize.css'
 import './index.css'
+import "@fontsource/lobster"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
