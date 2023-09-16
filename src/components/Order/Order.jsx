017d84c1e20fbb73b9d39bc5cc1491273267b132
misha133kv <mihail_fomin_95@mail.ru>
2023-09-16 16:40:15 +0500

    revert commits

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -5,62 +5,65 @@ import { useEffect, useState } from 'react'
 import { orderRequestAsync } from '../../store/order/orderSlice'
 import { openModal } from '../../store/modalDelivery/modalDeliverySlice'
 import classNames from 'classnames'
+import db from '../../assets/db'
 
 
 export const Order = () => {
-	const { totalPrice, totalCount, orderList, orderGoods } = useSelector(state => state.order)
-	const dispatch = useDispatch()
+  const { totalPrice, totalCount, orderList, orderGoods } = useSelector(state => state.order)
+  const dispatch = useDispatch()
 
-	const [openOrder, setOpentOrder] = useState(false)
+  const [openOrder, setOpentOrder] = useState(false)
 
-	useEffect(() => {
-		dispatch(orderRequestAsync())
-	}, [orderList.length])
+  useEffect(() => {
+    dispatch(orderRequestAsync())
+  }, [orderList.length])
 
-	return (
-		<div className={classNames(style.order, openOrder ? style.order_open : '')}>
-			<section className={style.wrapper}>
-				<div className={style.header}
-					tabIndex="0"
-					role="button"
-					onClick={() => setOpentOrder(!openOrder)}
-				>
-					<h2 className={style.title}>Корзина</h2>
+  return (
+    <div className={classNames(style.order, openOrder ? style.order_open : '')}>
+      <section className={style.wrapper}>
+        <div className={style.header}
+          tabIndex="0"
+          role="button"
+          onClick={() => setOpentOrder(!openOrder)}
+        >
+          <h2 className={style.title}>Корзина</h2>
 
-					<span className={style.count}>{totalCount}</span>
-				</div>
+          <span className={style.count}>{totalCount}</span>
+        </div>
 
-				<div className={style.wrap_list}>
-					<ul className={style.list}>
-						{orderGoods.map((item) => <OrderGoods key={item.id} {...item} />)}
-					</ul>
+        <div className={style.wrap_list}>
+          <ul className={style.list}>
 
-					<div className={style.total}>
-						<p>Итого</p>
-						<p>
-							<span className={style.amount}>{totalPrice}</span>
-							<span className={style.currency}>&nbsp;₽</span>
-						</p>
-					</div>
+            {orderGoods.map((item) => <OrderGoods key={item.id} {...item} />)}
 
-					<button className={style.submit}
-						disabled={orderGoods.length === 0}
-						onClick={() => dispatch(openModal())}
-					>
-						Оформить заказ
-					</button>
+          </ul>
 
-					<div className={style.apeal}>
-						<p className={style.text}>Стоимость доставки будет посчитана позже</p>
-						<button
-							className={style.close}
-							onClick={() => setOpentOrder(!openOrder)}						>
-							Свернуть
-						</button>
-					</div>
-				</div>
-			</section>
-		</div>
-	)
+          <div className={style.total}>
+            <p>Итого</p>
+            <p>
+              <span className={style.amount}>{totalPrice}</span>
+              <span className={style.currency}>&nbsp;₽</span>
+            </p>
+          </div>
+
+          <button className={style.submit}
+            disabled={orderGoods.length === 0}
+            onClick={() => dispatch(openModal())}
+          >
+            Оформить заказ
+          </button>
+
+          <div className={style.apeal}>
+            <p className={style.text}>Стоимость доставки будет посчитана позже</p>
+            <button
+              className={style.close}
+              onClick={() => setOpentOrder(!openOrder)}						>
+              Свернуть
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 
 }
