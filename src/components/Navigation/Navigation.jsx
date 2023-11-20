7d2fe92b01d8ac00fcc9572d6bf9907cfce4912b
misha133kv <mihail_fomin_95@mail.ru>
2023-11-20 23:00:52 +0500

    added translate to item

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -13,17 +13,10 @@ export const Navigation = () => {
 
   if (isLoading) return <h1>Loading...</h1>
 
-  // const { category, activeCategory } = useSelector((state) => state.category)
-
-  function getCategoryTranslate(categories) {
-    for (const category of categories) {
-      const categoryObject = categoryTranslations.find(el => el.title === category)
-      return categoryObject.rus
-    }
+  function getCategoryTranslate(category) {
+    const categoryObject = categoryTranslations.find(el => el.title === category)
+    return categoryObject.rus
   }
-
-
-  console.log(getCategoryTranslate(data));
 
   function getImageUrl(name) {
     return new URL(`../../assets/img/categories/${name}.png`, import.meta.url).href
@@ -42,28 +35,11 @@ export const Navigation = () => {
                 style={{ backgroundImage: `url(${getImageUrl(item)})` }}
                 onClick={() => dispatch(changeCategory({ indexCategory: i }))}
               >
-                {item}
+                {getCategoryTranslate(item) || item}
               </button>
             </li>
           )}
         </ul>
-
-        {/* <ul className={style.list}>
-          {category.map((item, i) =>
-            <li key={item.title} className={style.item}>
-              <button className={classNames(
-                style.button,
-                activeCategory === i ? style.button_active : '')}
-                style={{ backgroundImage: `url(${getImageUrl(item.image)})` }}
-                onClick={() => {
-                  dispatch(changeCategory({ indexCategory: i }))
-                }}
-              >
-                {item.rus}
-              </button>
-            </li>
-          )}
-        </ul> */}
       </Container>
     </nav>
   )
